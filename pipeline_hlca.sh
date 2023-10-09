3286246603d9f51823acc3dd5b65cecfbf5f3957
Vladimir Shitov <vladimir.shitov@helmholtz-muenchen.de>
2023-10-09 11:45:27 +0200

    Run python files to download data

--- pipeline_hlca.sh
+++ pipeline_hlca.sh
@@ -1,3 +1,3 @@
-bash download_HLCA.sh
+python -u download_HLCA.py >> logs_download_hlca.txt
 python -u preprocess_hlca.py >> logs_preprocess_hlca.txt
 python -u represent_hlca.py >> logs_represent_hlca.txt
