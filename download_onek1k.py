839eb11ff9e7f6e95e907e690c5cacb293bb1a91
Vladimir Shitov <vladimir.shitov@helmholtz-muenchen.de>
2023-10-09 11:22:20 +0200

    Fix path to onek1k

--- download_onek1k.py
+++ download_onek1k.py
@@ -3,5 +3,5 @@ import cellxgene_census
 CELLXGENE_ID = "dde06e0f-ab3b-46be-96a2-a8082383c4a1"
 
 cellxgene_census.download_source_h5ad(
-    CELLXGENE_ID, to_path="data/onek1k.h5ad"
+    CELLXGENE_ID, to_path="../data/onek1k.h5ad"
 )
