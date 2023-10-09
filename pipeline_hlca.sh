92a3652f1062b2b56242d277cbed7f2442641b65
Vladimir Shitov <vladimir.shitov@helmholtz-muenchen.de>
2023-10-09 11:33:39 +0200

    Add pipelines for HLCA and Onek1k

---
+++ pipeline_hlca.sh
@@ -0,0 +1,3 @@
+bash download_HLCA.sh
+python -u preprocess_hlca.py >> logs_preprocess_hlca.txt
+python -u represent_hlca.py >> logs_represent_hlca.txt
---
+++ pipeline_onek1k.sh
@@ -0,0 +1,3 @@
+bash preprocess_onek1k.sh
+python -u preprocess_onek1k.py >> logs_preprocess_onek1k.txt
+python -u represent_onek1k.py >> logs_represent_onek1k.txt
