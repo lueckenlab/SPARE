3e2ef5f3fa5bb136034dace78a6da9f24e3c5d7e
Vladimir Shitov <vladimir.shitov@helmholtz-muenchen.de>
2023-10-09 13:31:02 +0200

    Fix dataset ID

--- download_onek1k.py
+++ download_onek1k.py
@@ -1,6 +1,6 @@
 import cellxgene_census
 
-CELLXGENE_ID = "dde06e0f-ab3b-46be-96a2-a8082383c4a1"
+CELLXGENE_ID = "3faad104-2ab8-4434-816d-474d8d2641db"
 
 cellxgene_census.download_source_h5ad(
     CELLXGENE_ID, to_path="../data/onek1k.h5ad"
