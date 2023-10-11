0a3e1e83ef36f1decaf34b892b1ccdc9668526ab
Vladimir Shitov <vladimir.shitov@helmholtz-muenchen.de>
2023-10-11 17:49:01 +0200

    Overwrite logs and save them in a separate dir

--- pipeline_onek1k.sh
+++ pipeline_onek1k.sh
@@ -1,3 +1,3 @@
-python -u download_onek1k.py >> logs_download_onek1k.txt
-python -u preprocess_onek1k.py >> logs_preprocess_onek1k.txt
-python -u represent_onek1k.py >> logs_represent_onek1k.txt
+python -u download_onek1k.py > logs/ogs_download_onek1k.txt
+python -u preprocess_onek1k.py > logs/logs_preprocess_onek1k.txt
+python -u represent_onek1k.py > logs/logs_represent_onek1k.txt
