3e88f6dcd459216d19ef1933897f3d3b007ce254
farhadmd7 <m.mogharehdehkordi@gmail.com>
2024-05-02 21:19:19 +0200

    impl viash components for download_combat

---
+++ src/download_data/combat/download_combat.sh
@@ -0,0 +1,2 @@
+# mkdir -p ../data
+wget -P ../data https://zenodo.org/record/6120249/files/COMBAT-CITESeq-DATA.h5ad
---
+++ src/download_data/combat/pipeline_combat.sh
@@ -0,0 +1,5 @@
+mkdir -p logs
+
+# bash download_data.sh
+python -u preprocess_combat.py > logs/logs_preprocess_combat.txt
+python -u represent_combat.py > logs/logs_represent_combat.txt
