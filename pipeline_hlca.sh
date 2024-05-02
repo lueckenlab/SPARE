9ad5ebc3d706e37adef132ca2852bd1b1c706634
farhadmd7 <m.mogharehdehkordi@gmail.com>
2024-05-02 21:16:57 +0200

    removing the scripts to be put in new structure

--- pipeline_hlca.sh
+++
@@ -1,3 +0,0 @@
-python -u download_HLCA.py > logs/logs_download_hlca.txt
-python -u preprocess_hlca.py > logs/logs_preprocess_hlca.txt
-python -u represent_hlca.py > logs/logs_represent_hlca.txt
--- pipeline_onek1k.sh
+++
@@ -1,3 +0,0 @@
-python -u download_onek1k.py > logs/logs_download_onek1k.txt
-python -u preprocess_onek1k.py > logs/logs_preprocess_onek1k.txt
-python -u represent_onek1k.py > logs/logs_represent_onek1k.txt
