85578bfe39db32853b3303f4780a7b156861e4e9
farhadmd7 <m.mogharehdehkordi@gmail.com>
2024-06-12 18:38:19 +0200

    impl plotting the loss curves for VAEs

--- src/preprocess/script.py
+++ src/preprocess/script.py
@@ -5,6 +5,8 @@ import scvi
 import pandas as pd
 from patient_representation.pp import is_count_data
 import numpy as np
+import os
+import matplotlib.pyplot as plt
 
 random.seed(42)
 np.random.seed(42)
@@ -44,6 +46,12 @@ ADATA_PATH = par["input"]
 CELL_TYPE_KEY = par["cell_type_key"]
 BATCH_COVARIATES = par["batch_covariates"]
 BATCH_EFFECT = par["batch_effect_covariate"]
+
+figures_directory = os.path.join(os.path.dirname(par["output"]), "figures")
+
+if not os.path.exists(figures_directory):
+    os.makedirs(figures_directory)
+
 print("Reading data")
 adata = sc.read_h5ad(ADATA_PATH)
 
@@ -64,6 +72,24 @@ print("adata.layers['raw'].shape", adata.layers["raw"].shape)
 print("Running PCA")
 sc.tl.pca(adata)
 
+def plot_loss(history, loss_keys, title, filenames, counter):
+    plt.figure(figsize=(10, 5))
+    for loss_key, filename in zip(loss_keys, filenames):
+        if loss_key in history:
+            plt.plot(history[loss_key], label=f'{loss_key} (Train)')
+            plt.xlabel('Epoch')
+            plt.ylabel('Loss')
+            plt.title(title)
+            plt.legend()
+            # Add counter to filename
+            filename_with_counter = f"{counter}_{filename}"
+            full_path = os.path.join(figures_directory, filename_with_counter)
+            plt.savefig(full_path)
+            plt.close()
+            counter += 1
+    return counter
+
+counter = 1
 for batch_key in BATCH_COVARIATES:
     print(f"Obtain scVI and scanVI for batch key: {batch_key}")
     # Run scVI
@@ -73,6 +99,11 @@ for batch_key in BATCH_COVARIATES:
     embedding_name_scVI = f"X_scVI_{batch_key}"
     adata.obsm[embedding_name_scVI] = vae.get_latent_representation()
     print(f"Embedding stored: {embedding_name_scVI}")
+
+    counter = plot_loss(vae.history, 
+        ['reconstruction_loss_train', 'train_loss_epoch', 'elbo_train'], 
+        f'Training Loss Metrics for scVI ({batch_key})', 
+        [f'reconstruction_loss_scVI_{batch_key}.png', f'train_loss_epoch_scVI_{batch_key}.png', f'elbo_train_scVI_{batch_key}.png'], counter)
 
     # Run scANVI
     lvae = scvi.model.SCANVI.from_scvi_model(
@@ -85,6 +116,11 @@ for batch_key in BATCH_COVARIATES:
     embedding_name_scANVI = f"X_scANVI_{batch_key}"
     adata.obsm[embedding_name_scANVI] = lvae.get_latent_representation()
     print(f"Embedding stored: {embedding_name_scANVI}")
+
+    counter = plot_loss(vae.history, 
+    ['reconstruction_loss_train', 'train_loss_epoch', 'elbo_train'], 
+    f'Training Loss Metrics for scANVI ({batch_key})', 
+    [f'reconstruction_loss_scANVI_{batch_key}.png', f'train_loss_epoch_scANVI_{batch_key}.png', f'elbo_train_scANVI_{batch_key}.png'], counter)
 
 
 adata.write(par["output"], compression=par["output_compression"])
