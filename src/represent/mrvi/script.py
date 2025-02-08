bda953cc0ea0e55591eded1e73ff422b0b69062b
Vladimir Shitov <vladimir.shitov@helmholtz-muenchen.de>
2025-02-09 00:12:17 +0100

    Add more logs

--- src/represent/mrvi/script.py
+++ src/represent/mrvi/script.py
@@ -19,16 +19,24 @@ par = {
 }
 ## VIASH END
 
+print("Reading adata")
 adata = sc.read(par["input"])
+print(adata)
 
+print("Setting up the representation method")
 representation_method = pr.tl.MrVI(
     sample_key=par["sample_key"],
     cell_group_key=par["cell_type_key"],
     layer="X_raw_counts",
     max_epochs=par["max_epochs"],
 )
+
+print("Preparing the anndata object")
 representation_method.prepare_anndata(adata)
+
+print("Calculating the distance matrix")
 distances = representation_method.calculate_distance_matrix(force=True)
 
+print("Saving results")
 distances_df = pd.DataFrame(distances, index=representation_method.samples, columns=representation_method.samples)
 distances_df.to_csv(par["output"], index=True)
